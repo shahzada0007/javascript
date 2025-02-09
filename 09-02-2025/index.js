67a65ade478a046f96679124f3bee6021f81abd4
Muhammad Shahzad <146637482+shahzada0007@users.noreply.github.com>
2025-02-09 22:41:18 +0500

    Add files via upload

---
+++ 09-02-2025/index.js
@@ -0,0 +1,50 @@
+// console.log("Hello world")
+// alert("warning...")
+// document.write("hello world")
+
+// Illegal variables
+// var student name
+// var student-name
+// var 23student
+// var @#student
+// var stude#t
+//  var student-56
+
+//Legal variables
+// var studentName
+// var student_name
+// var stud91ent
+// var student56
+
+// var message = 10;
+
+// var message;
+// message = 20;
+// console.log(message);
+// var message;
+// console.log(message, "01");
+
+
+
+var amount = "3000";
+console.log(amount, "01")
+var amount = 32000.9;
+
+
+// var numberValue = 20;
+// var numberValue2 = 40;
+
+// numberValue = numberValue2
+
+// console.log(numberValue + numberValue2);
+
+console.log("not a number" / 2);
+
+
+// Data Types
+// Number  = 2323,343
+// String  = "hello", "2323"
+// BigInt  = 9007199254740991
+
+var message = 'hello'
+
