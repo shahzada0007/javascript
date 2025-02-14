26f5ee657b9058a2a410c8c8af0666c7a2f3d7d3
Muhammad Shahzad <146637482+shahzada0007@users.noreply.github.com>
2025-02-14 22:47:25 +0500

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,116 @@
+alert("hello world how are you")
+
+let message = 'Hello!'; // define the variable and assign the value
+
+alert(message); // Hello!
+//A variable is a “named storage” for data. //
+//let message;
+
+//message = 'Hello'; // store the string 'Hello' in the variable named message
+
+console.log("hello world")
+var student;
+student = 12;
+var studentRollNo = 242;
+var studentRollNo = 241;
+console.log(studentRollNo)
+// javascript data types//
+
+1//Number
+//let n = 123;
+//n = 12.345;////
+
+
+//strings//
+console.log("4563882")
+var names = "muhammad shahzad";
+console.log("names")
+//bolean//
+var compare = false;
+console.log(compare)
+//it shows true or false statment//
+var compare = 23 > 45;
+console.log(compare)
+
+
+//bigint//
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
+
+//it shows integer or just numbers or moe in number//
+//null//
+//it shows just empty or not existance//
+//undfined//
+//it defines just value exist but i cannot provide the value exists but i dont know //
+
+
+
+
+//null or undefined is a inerview question//
+
+
+
+
+
+//typeof data type//
+//The typeof operator
+//The typeof operator returns the type of the operand. It’s useful when we want to process values of different types differently or just want to do a quick check.
+
+//A call to typeof x returns a string with the type name://
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3)//
+
+
+
+
+
+
+
+
+
+
+console.log(typeof "12345")//string//
+console.log(typeof 1)//numbers//
+
+var newNumber = "222222";
+console.log(typeof Number(newNumber), "01");
+console.log(+newNumber), "01";
+//array//
+var array = []
+//object 
+var object = {}
+//PROMPT//
+//I SHOWS JUST TRUE OR FLASE//
+var userName = prompt("what is yor name");
+console.log(userName)//
+
+var number1 = prompt("122222");
+var number2 = prompt("122222");
+
+console.log(Number(number1) + (number2))
+//CONFIRM// 
+//IT JUST SHOWS TRUE OR FLASE //
+var isboss = confirm("ar you boss?");
+console.log(isboss)
+//OPERATORS ARE PERFORMED  WHEN TWO STRINGS NUMBERS ARE ADDED OR DIVIDED OPREATORS//
+//Numeric Conversion
+//Numeric conversion in mathematical functions and expressions happens automatically.//
+
+//For example, when division / is applied to non-numbers://
+
